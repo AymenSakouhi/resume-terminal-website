7474529badd26c928ee5169649e1118a21c3291c
Aymen Sakouhi <aymenmarkter@gmail.com>
2022-05-31 02:39:10 +0200

    first commit

---
+++ js/commands.js
@@ -0,0 +1,78 @@
+var youtube = "https://youtu.be/93B7t3GvDWg";
+var password = "mynameisjeff";
+var linkedin = "https://www.linkedin.com/in/sakouhiaymen/";
+var instagram = "https://www.instagram.com/rodndjok/";
+var github = "https://github.com/AymenSakouhi";
+var email = 'mailto:aymenmarketer@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Aymen!👋",
+  "I'm a software developer, who builds engaging websites like this one.",
+  "After graduating with a Master's in Computer Science and Mathematical Modeling, I worked professionally as a software engineer",
+  "building enterprise web applications for multiple companies such as IU university, Libertex and more.",
+  "I can adapt onto any technology rapidly making myself flexible upon any tech stack.",
+  "Passionate, diligent, continuously learning, 24/7 motivated and results-driven Node.js/ReactJS Full Stack developer with over 3 years of experience while grinding new techs.",
+  "Believe me, I even wrapped Elden Ring, Dark Souls and my super power is to cope with gaming lag and keeping calm while playing with screaming kids in valorant and CS.GO",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/sakouhiaymen' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/rodndjok' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/AymenSakouhi' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Aymen?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube video',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Aymen Sakouhi - A MERN Stack Developer</span>',
+  "         ___   ____",
+  "       /' --;^/ ,-_\\     \\ | /       ",
+  "      / / --o\\ o-\\ \\\\   --(_)--   ",
+  "     /-/-/|o|-|\\-\\\\|\\\\   / | \\   ",
+  "      '`  ` |-|   `` '            ___                                              _____            __                     __      _   ",
+  "            |-|                  /   |   __  __   ____ ___   ___    ____          / ___/  ____ _   / /__  ____   __  __   / /_    (_)  ",
+  "            |-|O                / /| |  / / / /  / __ `__ \\ / _ \\  / __ \\         \\__ \\  / __ `/  / //_/ / __ \\ / / / /  / __ \\  / /   ",
+  "            |-(\\,__            / ___ | / /_/ /  / / / / / //  __/ / / / /        ___/ / / /_/ /  / ,<   / /_/ // /_/ /  / / / / / /    ",
+  "         ...|-|\\--,\\_....     /_/  |_| \\__, /  /_/ /_/ /_/ \\___/ /_/ /_/        /____/  \\__,_/  /_/|_|  \\____/ \\__,_/  /_/ /_/ /_/     ",
+  "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.     /____/                                                                                         ",
+  "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
